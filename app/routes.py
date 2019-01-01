1c99fcf05aec84ff7ad7d9e1baeb8ef74afb10dd
vsvankhede <vijay.s.vankhede@gmail.com>
2019-01-01 08:01:50 +0530

    Dispaly inline html page

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username': 'Vijay'}
+	return '''
+	<html>
+		<head>
+			<title>Home Page - Microblog</title>
+		</head>
+		<body>
+			<h1>Hello, ''' + user['username'] + '''!</h1>
+		</body>
+	</html>
+	'''
 	
