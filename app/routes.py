8d593afc825a493e39a8a08948310bd59a281bf8
vsvankhede <vijay.s.vankhede@gmail.com>
2019-01-01 08:20:40 +0530

    Move inline html to templates

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'username': 'Vijay'}
-	return '''
-	<html>
-		<head>
-			<title>Home Page - Microblog</title>
-		</head>
-		<body>
-			<h1>Hello, ''' + user['username'] + '''!</h1>
-		</body>
-	</html>
-	'''
-	
+	return render_template('index.html', title='Home', user=user)
