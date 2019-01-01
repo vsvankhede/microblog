e85692d3d083e81d043e6489bb600bb8d85a6cca
vsvankhede <vijay.s.vankhede@gmail.com>
2019-01-01 08:40:47 +0530

    Display post to the users

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'username': 'Vijay'}
-	return render_template('index.html', title='Home', user=user)
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in India!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html', title='Home page', user=user, posts=posts)
